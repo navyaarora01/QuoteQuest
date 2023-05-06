1428593101e3912164f7d855a834ac0a3836efec
Navya <arora.navya2003@gmail.com>
2023-05-06 19:06:40 +0530

    commit

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 let realData = "";
+let quotesData="";
 const quotes = document.getElementById("quotes");
+const author = document.getElementById("author");
+const newQ = document.getElementById("newQ");
+const tweetMe = document.getElementById("tweetMe");
+
+const tweetNow = () =>{
+    let tweetPost = `https://twitter.com/intent/tweet?text=${quotesData.text}`;
+    window.open(tweetPost); 
+}
+
 const getNewQuotes = () => {
-	let rnum = Math.floor(Math.random() * 10);
-	// console.log(realData)
-	quotes.innerText = `${realData[rnum].text}`;
+	let rnum = Math.floor(Math.random() * 20);
+    quotesData = realData[rnum];
+	quotes.innerText = `${quotesData.text}`;
+    if(quotesData.author == null? (author.innerText = "unknown") : (author.innerText = `${quotesData.author}`));
+	// author.innerText = `${quotesData.author}`;
 };
 const getQuotes = async () => {
 	const api = "https://type.fit/api/quotes";
@@ -15,4 +27,6 @@ const getQuotes = async () => {
 		// console.log(realData);
 	} catch (error) {}
 };
+tweetMe.addEventListener("click" , tweetNow);
+newQ.addEventListener("click" , getNewQuotes);
 getQuotes();
